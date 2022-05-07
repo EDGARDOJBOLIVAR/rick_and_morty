91816355e65c678dac401fc7cf5014de5e57433b
edgardo_bolivar <82664365+EDGARDOJBOLIVAR@users.noreply.github.com>
2022-05-07 16:35:58 -0500

    Lectura de la API de rick and morty

---
+++ src/components/FilterByName.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="Buscar por nombre" @keyup="filter()" v-model="name">
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+export default {
+    setup(){
+        const store = useStore();
+        const name = ref('');
+
+        const filter = () =>{
+            store.dispatch('filterByName', name.value);
+        }
+
+        return {
+            filter, name
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.search{
+    width: 400px;
+    margin: 3rem auto 0;
+    input{
+        height: 53px;
+        width: 400px;
+        border: none;
+        border-radius: 10px;
+        padding: 0 0.5rem;
+    }
+}
+</style>
